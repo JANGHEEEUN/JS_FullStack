b6f6ae49a7caf518b1447507d483d1549855ce3a
JANGHEEEUN <qhqhclsrn@naver.com>
2020-03-20 21:36:45 +0900

    update

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,11 +1,14 @@
 import express from "express";
 import routes from "../routes";
 import { search, home } from "../controllers/videoController";
-import { login, join, logout } from "../controllers/userController";
+import { login, getJoin, logout, postJoin } from "../controllers/userController";
 
 const globalRouter = express.Router();
+
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
 globalRouter.get(routes.home, home);
-globalRouter.get(routes.join, join);
 globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 globalRouter.get(routes.search, search);
